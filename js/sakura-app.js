0e80998ab11475a70cc834ec53e1c0f10cd095f0
Gnomeshgh123 <1946424613@qq.com>
2023-03-06 17:08:22 +0800

    Site updated: 2023-03-06 17:08:21

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -180,10 +180,10 @@ function imgError (ele, type) {
       break
     case 3:
 
-      ele.src = 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/other/image-404.png'
+      ele.src = 'https://cdn.staticaly.com/gh/Gnomeshgh123/img@master/img/startdash/image-404.png'
       break
     default:
-      ele.src = 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/other/image-404.png'
+      ele.src = 'https://cdn.staticaly.com/gh/Gnomeshgh123/img@master/img/startdash/image-404.png'
   }
 }
 mashiro_global.post_list_show_animation = new function () {
